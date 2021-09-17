73803e6b7d7eed5cba1a76f6ad5c53e39cac69ea
radamr <rybicki-adam@wp.pl>
2021-09-17 19:17:23 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+let tempo = 0
+input.onButtonPressed(Button.A, function () {
+    tempo = 0
+    for (let index = 0; index < 8; index++) {
+        tempo += 50
+        music.playMelody("C5 B A G F E D C ", tempo)
+    }
+})
